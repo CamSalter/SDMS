d312af657372157ff8fc8598eab035ce12ec3290
Cam <102944566+CamSalter@users.noreply.github.com>
2023-02-12 13:20:55 -0500

    adding side pop-up navbar jsx and scss, with menu selectors

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,24 +4,51 @@ import logo from '../styles/assets/Docusec.png';
 import '../styles/LandingPage.scss';
 
 const LandingPage = () => {
-
   return (
     <div className='landing-page'>
-      <img src="" alt='DocuSec logo' className='logo' />
-      <p className='subtitle'>Your Information, Safe and Sound with DocuSec</p>
-      <p className='description'>
-        DocuSec uses the Trusted Document Format Security Protocol to keep your
-        information secure and protected. Say goodbye to worries and hello to
-        peace of mind with our top-notch document management system.{' '}
-      </p>
-      <div className='cta-section'>
-        <a href='/login' className='cta-button'>
-          Login
-        </a>
-        <a href='/signup' className='cta-button'>
-          Sign Up
-        </a>
-      </div>
+      <header className='header'>
+        <div className='container'>
+          <div className='row'>
+            <div className='brand-name-column'>
+              <h1 className='app-name'>__Docu-sec</h1>
+              <div class='hamburger-menu'>
+                <input id='menu__toggle' type='checkbox' />
+                <label class='menu__btn' for='menu__toggle'>
+                  <span></span>
+                </label>
+
+                <ul class='menu__box'>
+                  <li>
+                    <a class='menu__item' href='#'>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a class='menu__item' href='#'>
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a class='menu__item' href='#'>
+                      Team
+                    </a>
+                  </li>
+                  <li>
+                    <a class='menu__item' href='#'>
+                      Contact
+                    </a>
+                  </li>
+                  <li>
+                    <a class='menu__item' href='#'>
+                      Twitter
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
